e6d9cebea1def2c65506ee1a93119dfa1441164f
jimbojw <wilson.jim.r@gmail.com>
2023-05-10 19:51:50 -0400

    feat: Adding rudimentary library and related configuration.

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * @fileoverview Main entry point for Memorelay library.
+ */
+
+export class Memorelay {
+  // TODO(jimbo): Implement class to do something.
+}
