f5fc25e4b7008645ae55fb6be5490dfa533b0339
jimbojw <wilson.jim.r@gmail.com>
2023-05-11 09:05:04 -0400

    build: Adding ESLint dependency and related configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+/**
+ * @fileoverview Configuration for ESLint.
+ */
+
+const skipWords = `
+dotenv ecma fileoverview lang jsx memorelay msg nostr printf pubkey readonly req
+sig tsconfig tsx unparseable utf ws wss
+`.match(/\w+/g);
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/strict',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/no-confusing-void-expression': 'error',
+        '@typescript-eslint/prefer-readonly': 'error',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['spellcheck'],
+  root: true,
+  rules: {
+    'spellcheck/spell-checker': [
+      'warn',
+      {
+        comments: true,
+        strings: true,
+        identifiers: true,
+        templates: false,
+        lang: 'en_US',
+        skipWords: skipWords,
+        skipIfMatch: [/TODO\s*\([^)]+\):/, /[a-f0-9]{64}/],
+      },
+    ],
+  },
+};
