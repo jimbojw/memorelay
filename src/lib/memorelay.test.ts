d021a4afea58ca90de6756b8e48afa242d5b36e9
jimbojw <wilson.jim.r@gmail.com>
2023-05-12 11:27:16 -0400

    feat: Adding rumimentary Memorelay class behaviors.

--- src/lib/memorelay.test.ts
+++ src/lib/memorelay.test.ts
@@ -7,8 +7,62 @@
 
 import { Memorelay } from './memorelay';
 
+import { Event as NostrEvent } from 'nostr-tools';
+
+const EXAMPLE_SIGNED_EVENT: NostrEvent = Object.freeze({
+  content: 'BRB, turning on the miners',
+  created_at: 1683474317,
+  id: 'f9b1990f0c09f2f5bdd2311be939422ab2449f75d5de3a0167ba297bfb9ed9d3',
+  kind: 1,
+  pubkey: '6140478c9ae12f1d0b540e7c57806649327a91b040b07f7ba3dedc357cab0da5',
+  sig: 'c84d6e0db72b1b93f7ef95a03ad73519679966d007f030fe3e82fe66e199aa10278da0806109895b62c11f516dff986a59041461c6e26e600fa0f75f4948d8bd',
+  tags: [],
+});
+
 describe('Memorelay', () => {
-  it('should be a function', () => {
+  it('should be a constructor function', () => {
     expect(typeof Memorelay).toBe('function');
+
+    const memorelay = new Memorelay();
+    expect(memorelay instanceof Memorelay).toBe(true);
+  });
+
+  describe('addEvent', () => {
+    it('should return true for a previously unknown event', () => {
+      const memorelay = new Memorelay();
+      expect(memorelay.addEvent(EXAMPLE_SIGNED_EVENT)).toBe(true);
+    });
+
+    it('should return false when adding a known event', () => {
+      const memorelay = new Memorelay();
+      memorelay.addEvent(EXAMPLE_SIGNED_EVENT);
+      expect(memorelay.addEvent(EXAMPLE_SIGNED_EVENT)).toBe(false);
+    });
+  });
+
+  describe('hasEvent', () => {
+    it('should return false for an unknown event', () => {
+      const memorelay = new Memorelay();
+      expect(memorelay.hasEvent(EXAMPLE_SIGNED_EVENT)).toBe(false);
+    });
+
+    it('should return true for a known event', () => {
+      const memorelay = new Memorelay();
+      memorelay.addEvent(EXAMPLE_SIGNED_EVENT);
+      expect(memorelay.hasEvent(EXAMPLE_SIGNED_EVENT)).toBe(true);
+    });
+  });
+
+  describe('deleteEvent', () => {
+    it('should return false for an unknown event', () => {
+      const memorelay = new Memorelay();
+      expect(memorelay.deleteEvent(EXAMPLE_SIGNED_EVENT)).toBe(false);
+    });
+
+    it('should return true for a known event', () => {
+      const memorelay = new Memorelay();
+      memorelay.addEvent(EXAMPLE_SIGNED_EVENT);
+      expect(memorelay.deleteEvent(EXAMPLE_SIGNED_EVENT)).toBe(true);
+    });
   });
 });
--- src/lib/memorelay.ts
+++ src/lib/memorelay.ts
@@ -5,6 +5,45 @@
  * @fileoverview Main entry point for Memorelay library.
  */
 
+import { Event as NostrEvent } from 'nostr-tools';
+
 export class Memorelay {
-  // TODO(jimbo): Implement class to do something.
+  /**
+   * Map of events keyed by id known to this memorelay instance.
+   */
+  private readonly eventsMap = new Map<string, NostrEvent>();
+
+  /**
+   * Returns whether the provided event is in memory.
+   * @param event The event to check.
+   */
+  hasEvent(event: NostrEvent): boolean {
+    return this.eventsMap.has(event.id);
+  }
+
+  /**
+   * Add the given event to the events map and return whether successful.
+   * @param event The event to add.
+   * @returns Whether the event was added.
+   */
+  addEvent(event: NostrEvent): boolean {
+    if (this.hasEvent(event)) {
+      return false;
+    }
+    this.eventsMap.set(event.id, event);
+    return true;
+  }
+
+  /**
+   * Delete the event from the events map and return whether successful.
+   * @param event The event to add.
+   * @returns Whether the event was deleted.
+   */
+  deleteEvent(event: NostrEvent): boolean {
+    if (!this.hasEvent(event)) {
+      return false;
+    }
+    this.eventsMap.delete(event.id);
+    return true;
+  }
 }
