a3442f9719b5a1cae8bfa53065a908fba354e753
jimbojw <wilson.jim.r@gmail.com>
2023-05-15 13:20:36 -0400

    feat: Using Commander to implement --port and --log-level command line arguments.

--- src/bin.ts
+++ src/bin.ts
@@ -5,16 +5,60 @@
  * @fileoverview Entry point for Memorelay server (bin file).
  */
 
+import * as rawPackageJson from '../package.json';
+import { JSONSchemaForNPMPackageJsonFiles2 } from '@schemastore/package';
+const packageJson = rawPackageJson as JSONSchemaForNPMPackageJsonFiles2;
+
 import { MemorelayServer } from './lib/memorelay-server';
 
+import { program } from 'commander';
 import { createLogger, format, transports } from 'winston';
 
-// TODO(jimbo): Make these configurable.
-const PORT = 3000;
-const LOG_LEVEL = 'silly';
+if (!packageJson.name) {
+  throw new Error('name field missing from package.json');
+}
+
+if (!packageJson.description) {
+  throw new Error('description field missing from package.json');
+}
+
+if (!packageJson.version) {
+  throw new Error('version field missing from package.json');
+}
+
+program
+  .name(packageJson.name)
+  .description(packageJson.description)
+  .version(packageJson.version)
+  .option('-p, --port <number>', 'TCP port on which to listen', '3000')
+  .option('-l, --log-level <level>', 'Minimum log level to report', 'info');
+
+program.parse();
+
+const options = program.opts();
+
+const portNumber = +options.port;
+if (!Number.isFinite(portNumber)) {
+  throw new Error(`invalid port number: ${options.port as string}`);
+}
+
+const LOG_LEVELS = [
+  'error',
+  'warn',
+  'info',
+  'http',
+  'verbose',
+  'debug',
+  'silly',
+];
+
+const logLevel = options.logLevel as string;
+if (!LOG_LEVELS.includes(logLevel)) {
+  throw new Error(`unrecognized log level: '${logLevel}'`);
+}
 
 const logger = createLogger({
-  transports: [new transports.Console({ level: LOG_LEVEL })],
+  transports: [new transports.Console({ level: logLevel })],
   format: format.combine(
     format.colorize(),
     format.timestamp(),
@@ -25,7 +69,7 @@ const logger = createLogger({
   ),
 });
 
-const server = new MemorelayServer(PORT, logger);
+const server = new MemorelayServer(portNumber, logger);
 
 function reportErrorAndExit(error: unknown) {
   logger.log('error', error);
