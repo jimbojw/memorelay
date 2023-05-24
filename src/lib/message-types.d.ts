ce0277e175d0d3f7ea42a1ef19708cf1910048c4
jimbojw <wilson.jim.r@gmail.com>
2023-05-24 09:39:17 -0400

    feat: Include NIP-20 'OK' messages in message types.

--- src/lib/message-types.d.ts
+++ src/lib/message-types.d.ts
@@ -8,7 +8,26 @@
 import { Event as NostrEvent } from 'nostr-tools';
 import { Filter } from 'nostr-tools';
 
+// @see https://github.com/nostr-protocol/nips/blob/master/01.md
 export type EventMessage = ['EVENT', NostrEvent];
 export type ReqMessage = ['REQ', string, ...Filter[]];
 export type CloseMessage = ['CLOSE', string];
+export type EOSEMessage = ['EOSE', string];
+export type NoticeMessage = ['NOTICE', string];
+
+// @see https://github.com/nostr-protocol/nips/blob/master/20.md
+export type OKMessage = ['OK', string, boolean, string];
+
+/**
+ * A message that a client might send to a relay.
+ */
 export type ClientMessage = EventMessage | ReqMessage | CloseMessage;
+
+/**
+ * A message that a relay might send to a client.
+ */
+export type RelayMessage =
+  | EventMessage
+  | EOSEMessage
+  | NoticeMessage
+  | OKMessage;
