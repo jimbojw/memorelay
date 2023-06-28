a65f9b5043913cc7d3ef7bf530b1d6b7da87d32a
jimbojw <wilson.jim.r@gmail.com>
2023-06-28 14:48:44 -0400

    test: Adding tests for runRawMessageHandlers().

--- src/experimental/memorelay-client.test.ts
+++ src/experimental/memorelay-client.test.ts
@@ -41,4 +41,65 @@ describe('MemorelayClient', () => {
       expect(params[1]).toBe(false);
     });
   });
+
+  describe('runRawMessageHandlers', () => {
+    it('should return false if there are no handlers', async () => {
+      const request = {} as Request;
+      const webSocket = { on: jest.fn() } as unknown as WebSocket;
+      const memorelayClient = new MemorelayClient(webSocket, request);
+
+      const data = Buffer.from('EXAMPLE_BUFFER');
+      const isBinary = false;
+
+      const result = await memorelayClient.runRawMessageHandlers(
+        data,
+        isBinary
+      );
+
+      expect(result).toBe(false);
+    });
+
+    it('should should throw if a handler provides no buffer', async () => {
+      const request = {} as Request;
+      const webSocket = { on: jest.fn() } as unknown as WebSocket;
+      const memorelayClient = new MemorelayClient(webSocket, request);
+
+      memorelayClient.rawMessageHandlers.push((data, isBinary, next) => {
+        // Simulate user calling 'done' without providing a buffer.
+        (next as (status: string) => void)('done');
+      });
+
+      const data = Buffer.from('EXAMPLE_BUFFER');
+      const isBinary = false;
+
+      await expect(
+        memorelayClient.runRawMessageHandlers(data, isBinary)
+      ).rejects.toThrow('buffer');
+    });
+
+    it('should return handler-provided buffer', async () => {
+      const request = {} as Request;
+      const webSocket = { on: jest.fn() } as unknown as WebSocket;
+      const memorelayClient = new MemorelayClient(webSocket, request);
+
+      const replacementBuffer = Buffer.from('REPLACEMENT_DATA');
+      const replacementIsBinary = true;
+      memorelayClient.rawMessageHandlers.push((data, isBinary, next) => {
+        next('done', replacementBuffer, replacementIsBinary);
+      });
+
+      const data = Buffer.from('EXAMPLE_BUFFER');
+      const isBinary = false;
+
+      const result = await memorelayClient.runRawMessageHandlers(
+        data,
+        isBinary
+      );
+
+      expect(result).toEqual({
+        buffer: replacementBuffer,
+        isBinary: replacementIsBinary,
+      });
+    });
+  });
 });
