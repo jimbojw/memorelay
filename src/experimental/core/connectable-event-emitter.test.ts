7096432931ac071e48c2f74d18658cd0ef5c6bdf
jimbojw <wilson.jim.r@gmail.com>
2023-07-04 10:51:07 -0400

    feat: Adding ConnectableEventEmitter to use as base for MemorelayClient and MemorelayHub.

---
+++ src/experimental/core/connectable-event-emitter.test.ts
@@ -0,0 +1,87 @@
+/**
+ * @license SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * @fileoverview Tests for ConnectableEventEmitter.
+ */
+
+import { Handler } from '../types/handler';
+import { ConnectableEventEmitter, HANDLERS } from './connectable-event-emitter';
+
+describe('ConnectableEventEmitter', () => {
+  describe('connect()', () => {
+    it('should call setupHandlers() and store the result', () => {
+      const mockDisconnectFn = jest.fn<unknown, []>();
+      const mockHandler = { disconnect: mockDisconnectFn };
+      const mockSetupHandlersFn = jest.fn<Handler[], []>(() => [mockHandler]);
+
+      const connectableEventEmitter = new ConnectableEventEmitter(
+        mockSetupHandlersFn
+      );
+
+      expect(connectableEventEmitter.isConnected).toBe(false);
+      expect(connectableEventEmitter[HANDLERS]).not.toBeDefined();
+
+      const returnValue = connectableEventEmitter.connect();
+
+      expect(returnValue).toBe(connectableEventEmitter);
+      expect(connectableEventEmitter.isConnected).toBe(true);
+      expect(connectableEventEmitter[HANDLERS]).toEqual([mockHandler]);
+    });
+
+    it('should do nothing if already connected', () => {
+      const mockDisconnectFn = jest.fn<unknown, []>();
+      const mockHandler = { disconnect: mockDisconnectFn };
+      const mockSetupHandlersFn = jest.fn<Handler[], []>(() => [mockHandler]);
+
+      const connectableEventEmitter = new ConnectableEventEmitter(
+        mockSetupHandlersFn
+      );
+
+      connectableEventEmitter[HANDLERS] = [];
+
+      expect(connectableEventEmitter.isConnected).toBe(true);
+
+      connectableEventEmitter.connect();
+
+      expect(mockSetupHandlersFn).not.toBeCalled();
+    });
+  });
+
+  describe('disconnect()', () => {
+    it('should call handlers disconnect() callbacks and remove them', () => {
+      const mockDisconnectFn = jest.fn<unknown, []>();
+      const mockHandler = { disconnect: mockDisconnectFn };
+      const mockSetupHandlersFn = jest.fn<Handler[], []>(() => [mockHandler]);
+
+      const connectableEventEmitter = new ConnectableEventEmitter(
+        mockSetupHandlersFn
+      );
+
+      connectableEventEmitter[HANDLERS] = [mockHandler];
+
+      expect(mockDisconnectFn).not.toHaveBeenCalled();
+
+      const returnValue = connectableEventEmitter.disconnect();
+
+      expect(returnValue).toBe(connectableEventEmitter);
+      expect(connectableEventEmitter.isConnected).toBe(false);
+      expect(mockDisconnectFn).toHaveBeenCalledTimes(1);
+      expect(connectableEventEmitter[HANDLERS]).not.toBeDefined();
+    });
+
+    it('should do nothing if not connected', () => {
+      const mockDisconnectFn = jest.fn<unknown, []>();
+      const mockHandler = { disconnect: mockDisconnectFn };
+      const mockSetupHandlersFn = jest.fn<Handler[], []>(() => [mockHandler]);
+
+      const connectableEventEmitter = new ConnectableEventEmitter(
+        mockSetupHandlersFn
+      );
+
+      connectableEventEmitter.disconnect();
+
+      expect(mockDisconnectFn).not.toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/experimental/core/connectable-event-emitter.ts
@@ -0,0 +1,69 @@
+/**
+ * @license SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * @fileoverview A wrapper around BasicEventEmitter for performing complete
+ * connect() and disconnect().
+ */
+
+import { BasicEventEmitter } from './basic-event-emitter';
+import { Handler } from '../types/handler';
+
+/**
+ * Symbol for accessing the internal handlers list in tests.
+ */
+export const HANDLERS = Symbol('handlers');
+
+/**
+ * Created by a Memorelay instance, a MemorelayClient sits atop a WebSocket. It
+ * receives raw message events from the socket, and sends encoded messages back.
+ */
+export class ConnectableEventEmitter extends BasicEventEmitter {
+  private handlers?: Handler[];
+
+  /**
+   * @param setupHandlers Callback function to setup event handlers.
+   */
+  constructor(readonly setupHandlers: () => Handler[]) {
+    super();
+  }
+
+  get [HANDLERS](): Handler[] | undefined {
+    return this.handlers;
+  }
+
+  set [HANDLERS](handlers: Handler[] | undefined) {
+    this.handlers = handlers;
+  }
+
+  get isConnected(): boolean {
+    return this.handlers !== undefined;
+  }
+
+  /**
+   * Connect event handlers. This is delayed until connect() is called in order
+   * to give others a chance to listen first.
+   * @returns this
+   */
+  connect(): this {
+    if (!this.handlers) {
+      const { setupHandlers } = this;
+      this.handlers = setupHandlers();
+    }
+    return this;
+  }
+
+  /**
+   * Disconnect all event handlers.
+   * @returns this.
+   */
+  disconnect(): this {
+    if (this.handlers) {
+      this.handlers.forEach((handler) => {
+        handler.disconnect();
+      });
+      this.handlers = undefined;
+    }
+    return this;
+  }
+}
