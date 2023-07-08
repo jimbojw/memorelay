99e12d83d4b40b78ce32bb8b04427f8817b14888
jimbojw <wilson.jim.r@gmail.com>
2023-07-08 16:11:11 -0400

    feat: Adding NIP-11 support to default Memorelay.

--- src/memorelay.ts
+++ src/memorelay.ts
@@ -6,28 +6,49 @@
  */
 
 import { MemorelayHub } from './core/lib/memorelay-hub';
-import { RelayError } from './core/errors/relay-error';
-import { RelayEvent } from './core/events/relay-event';
 import { createClients } from './core/plugins/create-clients';
 import { basicProtocol } from './nip-0001-basic-protocol/plugins';
+import { relayInformationDocument } from './nip-0011-relay-information-document/plugins/relay-information-document';
 
 /**
  * Memorelay main class. Extends MemorelayHub and attaches default behavior.
+ *
+ * Note: You MUST call connect() to attach Memorelay's event handlers. Handlers
+ * are delayed until connect() to give plugins a chance to listen and be earlier
+ * in the event handling stack.
+ *
+ * Example usage with native Node http module:
+ *
+ *   const memorelay = new Memorelay().connect();
+ *   const httpServer = createServer(memorelay.handleRequest());
+ *   httpServer.on('upgrade', memorelay.handleUpgrade());
+ *   httpServer.listen({ port: 3000 });
+ *
+ * Example usage with Express:
+ *
+ *   const memorelay = new Memorelay().connect();
+ *   const app = express();
+ *   app.use('/', memorelay.handleRequest());
+ *   app.get('/', (req, res) => {
+ *     res.send('HELLO WORLD');
+ *   });
+ *   const server = app.listen(3000);
+ *   server.on('upgrade', memorelay.handleUpgrade());
  */
-export class Memorelay<
-  PluginRelayEvent extends RelayEvent = RelayEvent,
-  PluginRelayError extends RelayError = RelayError
-> extends MemorelayHub<
-  PluginRelayEvent | RelayEvent,
-  PluginRelayError | RelayError
-> {
+export class Memorelay extends MemorelayHub {
   constructor() {
+    // NOTE: EventEmitter listeners are invoked in the order in which they are
+    // added. So here we attach plugins in reverse order so that the core
+    // event handlers are attached last.
     super(() => [
+      // Implement NIP-11 relay information document requests.
+      relayInformationDocument(this),
+
+      // Implement NIP-01 basic Nostr protocol support.
+      basicProtocol(this),
+
       // Upgrade connected WebSockets to full MemorelayClient instances.
       createClients(this),
-
-      // Implement basic Nostr protocol support.
-      basicProtocol(this),
     ]);
   }
 }
